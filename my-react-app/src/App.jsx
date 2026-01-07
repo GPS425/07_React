b6b6299c98b753355b28bec23d95f9b0360e8275
GPS425 <jjmk010425@gmail.com>
2026-01-07 09:46:12 +0900

    상태 내리꽂기

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css' // >> CSS import해야 적용 됨
 import Exam1 from './components/Exam1'
 import Exam2 from './components/Exam2'
+import Exam3 from './components/Exam3'
 
 function App() {
   //const [count, setCount] = useState(0)
@@ -21,6 +22,7 @@ function App() {
       {showExam && <Exam2 ddiyong="hello" test="world"/>}*/}
       {/* 조건부 렌더링 : 조건에 따라 렌더링되는 방법 && 앞에 있는 showExam이 true면 Exam1 렌더링됨
                                                     false면 Exam1 렌더링되지 않음 */}
+      <Exam3 />
     </>
   )
 }
